a1c3ccfd8dd91a96225d0d4b58cb1b62e7fc4004
luke-Uni <82521845+luke-Uni@users.noreply.github.com>
2022-05-11 19:37:34 +0200

    new comment

--- Dech-board/src/main/java/dech/board/user/UserService.java
+++ Dech-board/src/main/java/dech/board/user/UserService.java
@@ -6,30 +6,33 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-	
-ArrayList<User> addUser = new ArrayList<>();
-public void addUser (User user) {
-	addUser.add(user);
-}
 
-//Find me
-public int findUser(String username) {
-	for (int i = 0; i < addUser.size(); i++) {
-		if (addUser.get(i).getUsername().toLowerCase().equals(username.toLowerCase())) {
-			return i + 1;
-			// if the fighter is found, i is increased by one (when using the method, i must
-			// be reduced by one again)
-		}
+	ArrayList<User> addUser = new ArrayList<>();
 
+	public void addUser(User user) {
+		addUser.add(user);
 	}
-	return 0;
-}
-public ArrayList<User> getAddUser() {
-	return addUser;
-}
-public void setAddUser(ArrayList<User> addUser) {
-	this.addUser = addUser;
-}
-	
+
+	// Find me
+	public int findUser(String username) {
+		for (int i = 0; i < addUser.size(); i++) {
+			if (addUser.get(i).getUsername().toLowerCase().equals(username.toLowerCase())) {
+				return i + 1;
+				// if the fighter is found, i is increased by one (when using the method, i must
+				// be reduced by one again)
+			}
+
+		}
+		return 0;
+	}
+
+	// Yes
+	public ArrayList<User> getAddUser() {
+		return addUser;
+	}
+
+	public void setAddUser(ArrayList<User> addUser) {
+		this.addUser = addUser;
+	}
 
 }
