13591d251dcefd2ce67c8a4f47ceb003e8775529
arvandh439 <arvandhashemi@gmail.com>
2022-05-12 17:13:45 +0200

    we did the basic structure for the comment

--- Dech-board/src/main/java/dech/board/comment/repository/Commentrepository.java
+++ Dech-board/src/main/java/dech/board/comment/repository/Commentrepository.java
@@ -1,5 +1,24 @@
 package dech.board.comment.repository;
 
-public class Commentrepository {
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Repository;
+
+import dech.board.comment.model.Comment;
+@Repository
+public class CommentRepository{
+
+
+   ArrayList<Comment> comments = new ArrayList <>();
+
+
+   public ArrayList<Comment> getComments() {
+		
+		
+    return comments;
+}
+
+    
+
     
 }
--- Dech-board/src/main/java/dech/board/comment/service/Commentservice.java
+++ Dech-board/src/main/java/dech/board/comment/service/Commentservice.java
@@ -1,5 +1,29 @@
 package dech.board.comment.service;
 
-public class Commentservice {
-    
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dech.board.comment.model.Comment;
+import dech.board.comment.repository.CommentRepository;
+
+@Service
+public class CommentService {
+    @Autowired
+	CommentRepository cmt= new CommentRepository();
+	
+	//method to ad a post
+	public void addComment(Comment comment) {
+		
+		cmt.getComments().add(comment);
+		
+		
+		
+	}
+	public ArrayList<Comment> getComments() {
+		
+		
+		return cmt.getComments();
+	}
 }
