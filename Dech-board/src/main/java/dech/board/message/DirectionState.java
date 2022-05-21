40a74b3389cdbfa356288cea53b5f85e8abe2547
RYChiy <85196512+RYChiy@users.noreply.github.com>
2022-05-21 13:01:10 +0200

    Add files via upload

---
+++ Dech-board/src/main/java/dech/board/message/DirectionState.java
@@ -0,0 +1,6 @@
+package dech.board.message;
+
+public enum DirectionState {
+
+	SEND, RECEIVED
+}
---
+++ Dech-board/src/main/java/dech/board/message/MessageRepository.java
@@ -0,0 +1,14 @@
+package dech.board.message;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class MessageRepository {
+
+	
+	ArrayList<Message> messageList = new ArrayList<>();
+	
+}
