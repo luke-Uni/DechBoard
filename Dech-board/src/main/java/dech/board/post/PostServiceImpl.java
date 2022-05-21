67ee774e707208a7eb6b82a3fa76bdb58fa5f045
RYChiy <85196512+RYChiy@users.noreply.github.com>
2022-05-21 11:27:43 +0200

    Update PostServiceImpl.java

--- Dech-board/src/main/java/dech/board/post/PostServiceImpl.java
+++ Dech-board/src/main/java/dech/board/post/PostServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PostServiceImpl implements MessageService {
+public class PostServiceImpl  {
 
 	@Autowired
 	PostRepository messageRepo = new PostRepository();
