58ee2ea361fdd499bbb1e7df8ecd327cce5b412c
luke-Uni <82521845+luke-Uni@users.noreply.github.com>
2022-05-27 20:10:41 +0200

    generateToken

--- Dech-board/src/main/java/dech/board/confirmation/ConfirmationService.java
+++ Dech-board/src/main/java/dech/board/confirmation/ConfirmationService.java
@@ -1,8 +1,24 @@
 package dech.board.confirmation;
 
+import java.lang.annotation.Retention;
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConfirmationService {
-    
+
+    public String generateToken() {
+
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 10 + 1);
+
+        String token = "";
+
+        while (token.length() <= 5) {
+            token += randomNum;
+        }
+
+        return token;
+    }
+
 }
