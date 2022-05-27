e36053914bf5725b71cf7277791737deeb07cda1
luke-Uni <82521845+luke-Uni@users.noreply.github.com>
2022-05-27 20:17:35 +0200

    formatting

--- Dech-board/src/test/java/dech/board/DechBoardApplicationTests.java
+++ Dech-board/src/test/java/dech/board/DechBoardApplicationTests.java
@@ -3,9 +3,6 @@ package dech.board;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import dech.board.confirmation.Confirmation;
-import dech.board.confirmation.ConfirmationService;
-
 @SpringBootTest
 class DechBoardApplicationTests {
 
